4b8378d1b2c1f49bcbea56de6a70f4550dabec9f
MadelynDoyle <doylemr@email.sc.edu>
2023-11-12 22:24:26 -0500

    pls lord up above

---
+++ public/script.js
@@ -0,0 +1,67 @@
+const getBooks = async () => {
+    try {
+      return (await fetch("api/recipes/")).json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+const showBooks = async () => {
+    try {
+        const booksJSON = await getBooks();
+        const bookDiv = document.getElementById("bookContainer");
+
+        if (booksJSON && booksJSON.length > 0) {
+            booksJSON.forEach((book) => {
+                const section = document.createElement("div");
+                section.classList.add("book");
+
+                const img = document.createElement("img");
+                img.src = book.img;
+                img.classList.add("book-image");
+                section.appendChild(img);
+
+                const bookInfo = document.createElement("div");
+                bookInfo.classList.add("book-info");
+                section.appendChild(bookInfo);
+
+                const h2 = document.createElement("h2");
+                h2.textContent = book.title;
+                bookInfo.appendChild(h2);
+
+                const author = document.createElement("p");
+                author.textContent = `Author: ${book.author}`;
+                bookInfo.appendChild(author);
+
+                const genre = document.createElement("p");
+                genre.textContent = `Genre: ${book.genre}`;
+                bookInfo.appendChild(genre);
+
+                const rating = document.createElement("p");
+                rating.textContent = `Rating: ${book.rating}`;
+                bookInfo.appendChild(rating);
+
+                const characters = document.createElement("p");
+                characters.textContent = `Main Characters: ${book.maincharacters.join(', ')}`;
+                bookInfo.appendChild(characters);
+
+                bookDiv.appendChild(section);
+            });
+        } else {
+            console.log("No books found");
+        }
+    } catch (error) {
+        console.log("Error retrieving or displaying books:", error);
+    }
+};
+
+  
+
+  window.onload = () => {
+    showRecipes();
+    document.getElementById("book-form").onsubmit = addEditRecipe;
+    document.getElementById("add-book-container").onclick = showHideAdd;
+  
+    document.querySelector(".close").onclick = () => {
+      document.querySelector(".dialog").classList.add("transparent");
+    };
+  };
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const server = express();
+const Joi = require("joi");
+server.use(express.static("public"));
+server.use(express.json());
+const cors = require("cors");
+server.use(cors());
+
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+server.get("/api/books", (req, res) => {
+    const books = [];
+    books[0] = {
+        title: "Heir Of Fire",
+        author: "Sarah J Maas",
+        genre: "Fantasy",
+        img: "images/heiroffire.jpg",
+        rating: "9/10",
+        maincharacters: ["Aelin","Rowan", "Manon", "Dorian"]
+    };
+    books[1] = {
+        title: "Queen Of Shadows",
+        author: "Sarah J Maas",
+        genre: "Fantasy",
+        img: "images/queenofshadows.jpg",
+        rating: "6/10",
+        maincharacters: ["Aelin","Rowan", "Manon", "Dorian"]
+    };
+    books[2] = {
+        title: "Empire Of Storms",
+        author: "Sarah J Maas",
+        genre: "Fantasy",
+        img: "images/empireofstorms.jpg",
+        rating: "7/10",
+        maincharacters: ["Aelin","Rowan", "Manon", "Dorian"]
+    };
+    books[3] = {
+        title: "Kingdom of Ash",
+        author: "Sarah J Maas",
+        genre: "Fantasy",
+        img: "images/kingdomofash.jpg",
+        rating: "8/10",
+        maincharacters: ["Aelin","Rowan", "Manon", "Dorian"]
+    };
+    books[4] = {
+        title: "Fourth Wing",
+        author: "Rebecca Yarros",
+        genre: "Fantasy",
+        img: "images/fourthwing.jpg",
+        rating: "9/10",
+        maincharacters: ["Violet","Xaden", "Tairn", "Liam"]
+    };
+
+    res.json(books);
+});
+
+server.listen(3000, () => {
+    console.log("listening");
+});
