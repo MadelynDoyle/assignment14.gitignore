12937d17551908b10679282d3bb3bf1dec212305
MadelynDoyle <doylemr@email.sc.edu>
2023-11-13 11:08:00 -0500

    img better work

--- public/script.js
+++ public/script.js
@@ -55,14 +55,69 @@ const showBooks = async () => {
     }
 };
 
+const addBook = async () => {
+    try {
+  
+        const title = document.querySelector('#book-form input[name="title"]').value;
+        const author = document.querySelector('#book-form input[name="author"]').value;
+        const genre = document.querySelector('#book-form input[name="genre"]').value;
+        const img = document.querySelector('#book-form input[name="img"]').value; // Update this based on your form structure
+        const rating = document.querySelector('#book-form input[name="rating"]').value;
+        const maincharacters = document.querySelector('#book-form input[name="maincharacters"]').value.split(',').map(str => str.trim());
+
+        if (!title || !author || !genre || !img || !rating || !maincharacters) {
+          alert('Please fill in all required fields.');
+          return;
+        }
+    
+
+        const response = await fetch("/api/books", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            author,
+            genre,
+            img,
+            rating,
+            maincharacters,
+          }),
+        });
+    
+        if (response.ok) {
+          showBooks();
+        } else {
+          alert('Failed to add the book. Please try again.');
+        }
+    
+      } catch (error) {
+        console.error("Error adding book:", error);
+      } finally {
+        hideAddBook(); 
+      }
+  };
+  const showAddBook = () => {
+    console.log("Showing add book form");
+    document.getElementById("add-book-container").classList.remove("transparent");
+  };
+  
+  const hideAddBook = () => {
+    console.log("Hiding add book form");
+    document.getElementById("add-book-container").classList.add("transparent");
+  };
   
 
-  window.onload = () => {
+window.onload = () => {
     showBooks();
-    document.getElementById("book-form").onsubmit = addEditRecipe;
-    document.getElementById("add-book-container").onclick = showHideAdd;
+    document.getElementById("book-form").onsubmit = addBook;
+    document.getElementById("add-book").onclick = showAddBook;
   
     document.querySelector(".close").onclick = () => {
-      document.querySelector(".dialog").classList.add("transparent");
+      hideAddBook();
     };
-  };
+    document.getElementById("add-book").onclick = showAddBook;
+    document.getElementById("add-book-container").onclick = hideAddBook;
+    document.querySelector(".close").onclick = hideAddBook;
+};
